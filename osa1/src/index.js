8dbacce5cfd32921bc58588ce724c2eb4e464d9c
Juhani Rönnberg <juhani.ronnberg@gmail.com>
2018-08-09 14:49:31 +0300

    tehtävä 1.12

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,144 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-  
-
 class App extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        good_counter: 0,
-        neutral_counter: 0,
-        bad_counter: 0,
-        allAnswers: []
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 0
     }
-    statistics = (props) => {
-         if (this.state.allAnswers.length === 0) {
-            return (
-                <div>
-                  <p>yhtään palautetta ei ole annettu</p>
-                </div>
-              )
-        } 
-        
-        return (
+  }
+
+  nextAnekdote = () => () => {
+
+    
+    this.setState({  
+        selected: Math.floor(Math.random() * anecdotes.length),
+    })
+
+
+  }
+
+  render() {
+    return (
+      <div>
             <div>
-                <h1>statistiikka</h1>
-                <table>
-                    
-                    <tbody>
-                        <tr>
-                            <td>hyvä</td><td> {this.state.good_counter}</td>
-                        </tr>
-                        <tr>
-                            <td>neutraali</td><td> {this.state.neutral_counter}</td>
-                        </tr>
-                        <tr>
-                            <td>huono</td><td> {this.state.bad_counter}</td> 
-                        </tr>      
-                        
-                    </tbody>
-                </table>
-                <this.statistic/>
+                {this.props.anecdotes[this.state.selected]}
             </div>
-            )
-    } 
-
-    statistic = (props) => {
-        let yhtl = 0
-        let avg = 0
-        let pos = 0
-        let posPercentage = 0
-
-        
-        this.state.allAnswers.forEach((luku) => {
-                yhtl += luku
-                
-               avg = yhtl /  this.state.allAnswers.length
-               //console.log('yhte', yhtl, ' avg ', avg)
-               if (luku === 1) {
-                pos += 1 
-               }
-               posPercentage = (pos / this.state.allAnswers.length) * 100
-        })
-
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Avg</td><td> {avg.toFixed(2)}</td>
-                    </tr>
-                    <tr>
-                        <td>Positiivisia</td><td> {posPercentage.toFixed(2)} %</td>
-                    </tr>
-                </tbody>
-            </table>
-        )
-        
-    }
-   
-    
-    
-    kasvataYhdella = (arvo) => () => {
-        
-        
-
-        console.log('arvo', arvo)
-        
-
-        if (arvo === 'good_counter'){
-            this.setState({          
-                good_counter: this.state.good_counter + 1,
-                allAnswers: this.state.allAnswers.concat(1)
-            
-            
-            })
-        }
-        if (arvo === 'neutral_counter'){
-            this.setState({  
-                neutral_counter: this.state.neutral_counter + 1,
-                allAnswers: this.state.allAnswers.concat(0)
-            
-            })
-        }
-        if (arvo === 'bad_counter'){
-            this.setState({  
-                bad_counter: this.state.bad_counter + 1,
-                allAnswers: this.state.allAnswers.concat(-1)
-            
-            })
-        }  
-    }
-    
-
-    render() {
-        
-      return (
-        <div> 
-            <h1>Anna palautetta</h1>
-          <div>
-            <button onClick={this.kasvataYhdella('good_counter')}>
-                hyvä
-            </button>
-            <button onClick={this.kasvataYhdella('neutral_counter')}>
-              neutraali
-            </button>
-            <button onClick={this.kasvataYhdella('bad_counter')}>
-              huono
-            </button>
-            <div>
-                <this.statistics/>
-            </div>
-          </div>
+        <div>
+          <button onClick={this.nextAnekdote()}>Next anekdote </button>    
         </div>
-      )
-    }
-
+      </div>
+    
+      
+    )
+  }
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <App />,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
