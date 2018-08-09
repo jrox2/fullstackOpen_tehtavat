4c66503299c0cd20e6aadff3f045fdb063852267
Juhani Rönnberg <juhani.ronnberg@gmail.com>
2018-08-09 14:44:26 +0300

    tehtävä 1.6

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,62 +1,83 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Otsikko = (props) => {
-    return (
-      <div>
-        <h1>{props.kurssi}</h1>
-      </div>
-    )
+
+
+
+
+
+  
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        good_counter: 0,
+        neutral_counter: 0,
+        bad_counter: 0,
+        total_counter: 0
+      }
+    }
+  
+    statistics = (props) => {
+        return (
+            <div>
+                <h1>statistiikka</h1>
+                <div>hyvä {this.state.good_counter}</div>
+                <div>neutraali {this.state.neutral_counter}</div>
+                <div>huono {this.state.bad_counter}</div> 
+                <div>total {this.state.total_counter}</div>  
+                <div>
+                <p>yhteensä {props.yht} tehtävää</p>
+                </div>
+            </div>
+            )
+    } 
+
+    klikGood = () => {
+        this.setState({
+         good_counter: this.state.good_counter + 1,
+         total_counter: this.state.total_counter + this.state.good_counter
+         
+
+        })
+        
+    }
+    klikNeutral = () => {
+        this.setState({
+            neutral_counter: this.state.neutral_counter + 1   
+           })
+    }
+    klikBad = () => {
+        this.setState({
+            bad_counter: this.state.bad_counter + 1   
+           })
+    }
+    
+
+    render() {
+      return (
+        <div>
+          
+          <div>
+            <button onClick={this.klikGood}>
+              hyvä
+            </button>
+            <button onClick={this.klikNeutral}>
+              neutraali
+            </button>
+            <button onClick={this.klikBad}>
+              huono
+            </button>
+            <this.statistics/>
+          </div>
+        </div>
+      )
+    }
   }
 
-const Sisalto = (props) => {
-    return (
-        <div>
-             <p>{props.osat[0].nimi} {props.osat[0]. tehtavia}</p>
-        </div>
-        )
-}
+ 
 
-
-const Yhteensa = (props) => {
-    return (
-        <div>
-        <p>yhteensä {props.yht} tehtävää</p>
-        </div>
-        )
-}
-
-const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat = [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14
-        }
-      ]
-
-  return (
-    <div>
-      <Otsikko kurssi={kurssi}/>
-     
-     <Sisalto osat={osat}/>
-     {/*  <Sisalto osa={osat[0].nimi} tehtavia={osat[0].tehtavia}/>
-      <Sisalto osa={osat[1].nimi} tehtavia={osat[1].tehtavia}/>
-      <Sisalto osa={osat[2].nimi} tehtavia={osat[2].tehtavia}/>
- */}
-      <Yhteensa yht={osat[0].tehtavia + osat[1].tehtavia + osat[2].tehtavia}/>
-     
-    </div>
-  )
-}
 
 ReactDOM.render(
   <App />,
